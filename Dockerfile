b167a8a3e579cf87c8c129f5183cfcd6bb89c3eb
Michael Werner Gerstenberger <GersteM5@basfad.basf.net>
2020-03-06 13:22:14 +0100

    call django from api.py

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
-FROM python:3.7
+FROM registry.roqs.basf.net/base-images/ubuntu-python3-flaskrestplus:latest
 
 ADD . .
 EXPOSE 5000
 RUN GIT_SSL_NO_VERIFY=1 pip3 install -r requirements.txt
 RUN ls -la
-CMD ["python", "./manage.py", "runserver", "0.0.0.0:5000"]
+
+COPY api.py api.py
--- api.py
+++ api.py
@@ -1,30 +1,7 @@
-from flask import Flask, Blueprint, url_for, session
-from flask_restplus import Api, Resource, fields,apidoc
-from flask_cors import CORS
-import os, uuid
+import os
+for x in range(1000):
+    print("Hello world")
 
-app = Flask(__name__)
-app.secret_key = str(uuid.uuid4()) # required by "session"
-CORS(app, supports_credentials=True)
+os.system("python ./manage.py runserver 0.0.0.0:5000")
 
-
-if 'REVERSE_PROXY_REQUIRED' in os.environ:
-    from myProxyFix import ReverseProxied
-    app.wsgi_app = ReverseProxied(app.wsgi_app)
-
-api=Api(app,
-        title="prai_information_desk",
-        default="Endpoint",
-        default_label="",
-)
-
-@api.route('/ping')
-class Ping(Resource):
-    def get(self):
-        """
-        A simple GET endpoint
-        """
-        return {'ping':'PONG'}
-
-if __name__ == "__main__":
-    app.run(debug=True)
+#CMD ["python", "./manage.py", "runserver", "0.0.0.0:5000"]
