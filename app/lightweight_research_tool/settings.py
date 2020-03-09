e4dc651398196c675ad7389e45efcc376b095348
Michael Werner Gerstenberger <GersteM5@basfad.basf.net>
2020-03-09 18:52:36 +0100

    use force_script_name

--- app/lightweight_research_tool/settings.py
+++ app/lightweight_research_tool/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = "/static"#os.path.join(BASE_DIR, "static/")
-
+FORCE_SCRIPT_NAME = '/prai_information_desk'
 AUTH_USER_MODEL = 'users.Person'
 
 
