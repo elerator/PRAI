7fabc74218a5a8770d43e14c42d0c184ba15d2f5
Michael Werner Gerstenberger <GersteM5@basfad.basf.net>
2020-03-10 10:31:19 +0100

    only USE_X_FORWARDED_HOST = True

--- app/lightweight_research_tool/settings.py
+++ app/lightweight_research_tool/settings.py
@@ -19,7 +19,7 @@ STATIC_ROOT = "/static"#os.path.join(BASE_DIR, "static/")
 
 
 USE_X_FORWARDED_HOST = True
-FORCE_SCRIPT_NAME = 'prai_information_desk'
+#FORCE_SCRIPT_NAME = 'prai_information_desk'
 
 """
 
