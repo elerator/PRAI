dfcf60039fb0cfb69b992a307ab259c39a4c06a7
Michael Werner Gerstenberger <GersteM5@basfad.basf.net>
2020-03-10 12:39:48 +0100

    another try

--- app/lightweight_research_tool/settings.py
+++ app/lightweight_research_tool/settings.py
@@ -14,13 +14,13 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = "/static"#os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 
 
 #USE_X_FORWARDED_HOST = True
 FORCE_SCRIPT_NAME = 'https://app-dev.roqs.basf.net/prai_information_desk/'
-#FORCE_SCRIPT_NAME = "http://127.0.0.1:8009/"
+#FORCE_SCRIPT_NAME = "/"
 
 """
 
