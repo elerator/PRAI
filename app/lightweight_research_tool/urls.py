f842509ab959852a8cccb504beeb102064dd4680
Michael Werner Gerstenberger <GersteM5@basfad.basf.net>
2020-03-10 15:45:59 +0100

    a

--- app/lightweight_research_tool/urls.py
+++ app/lightweight_research_tool/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +26,4 @@ urlpatterns = [
     path('kanban/', include("kanban.urls")),
     path('capacities/', include("capacities.urls")),
     path('users/', include("users.urls")),
-]
+] + static(settings.STATIC_SUFFIX, document_root=settings.STATIC_ROOT)
