80d8e076b19a340cbdddc5b27c47e400e7ae600e
Michael Werner Gerstenberger <GersteM5@basfad.basf.net>
2020-03-10 13:26:31 +0100

    another try

--- app/lightweight_research_tool/settings.py
+++ app/lightweight_research_tool/settings.py
@@ -17,8 +17,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 #USE_X_FORWARDED_HOST = True
-#FORCE_SCRIPT_NAME = 'https://app-dev.roqs.basf.net/prai_information_desk/'
-FORCE_SCRIPT_NAME = "/"
+FORCE_SCRIPT_NAME = 'https://app-dev.roqs.basf.net/prai_information_desk/'
+#FORCE_SCRIPT_NAME = "/"
 
 LOGIN_URL = FORCE_SCRIPT_NAME+'login_required'
 
