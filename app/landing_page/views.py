d744c8a926627e0110eecc028b7503ebab528d41
Michael Werner Gerstenberger <GersteM5@basfad.basf.net>
2020-03-16 10:10:57 +0100

    login

--- app/landing_page/views.py
+++ app/landing_page/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as django_login
-from django.contrib.auth.models import User
-
 from django.http import HttpResponseRedirect, HttpResponse
 from users.models import Person
 from django.utils.datastructures import MultiValueDictKeyError
@@ -13,43 +11,11 @@ from urllib.parse import quote, unquote
 import os
 import requests
 
-#import basf_auth
-
 def landing_page(request):
     return render(request, 'landing_page/landing.html', {})
 
-"""def check_auth(request):
-    session_federation = session.get('basf_federation')
-    cookie_federation_cn = request.COOKIES.get('basf_federation_cn')
-    cookie_federation_access_token = request.COOKIES.get('basf_federation_access_token')
-    auth_header = request.headers.get('Authorization')
-    auth_result = basf_auth.check_authentication(session_federation, cookie_federation_cn, cookie_federation_access_token, auth_header)
-    if auth_result:
-        session['basf_federation'] = auth_result
-
-    return auth_result"""
-
-"""
-import requests
-import os
-
-
-TOKEN2INFO_URL = 'https://app.roqs.basf.net/auth/token2info'
-LOGIN_URL = 'https://app.roqs.basf.net/auth/login.html?redirect_uri={}'
-LOGOUT_URL = 'https://app.roqs.basf.net/auth/logout.html'
-
-
-def token2info(access_token):
-    r = requests.post(TOKEN2INFO_URL, data={'token': access_token}, verify=False, timeout=30)
-    assert r.status_code == 200
-    json = r.json()
-    return json if 'error' not in json else None
-
-
-"""
 token_to_info_url = 'https://app.roqs.basf.net/auth/token2info'
 post_basf_login_callback = "https://app.roqs.basf.net/auth/login.html?redirect_uri=https%3A%2F%2Fapp-dev.roqs.basf.net%2Fprai_information_desk%2Fafter_login"
-
 
 def login_view(request):
     try:
@@ -70,35 +36,20 @@ def after_login(request):
     assert r.status_code == 200
     session_federation = r.json()
 
-    #session_federation = token2info(cookie_federation_access_token)
     if not 'error' in session_federation and session_federation['user_id'] == cookie_federation_cn:
-        redirect_url = request.GET["next"]
-        return HttpResponseRedirect(redirect_url)
-        
-    return HttpResponse(str(session_federation)) # if session_federation --> User is authentificated
+        #User is authenticated
+        user = Person.objects.filter(username__iexact=cookie_federation_cn)
+        if user.exists():
+            #User is member of production ai
+            django_login(request, user)
+            return HttpResponseRedirect(redirect_url)
 
-    #return HttpResponse(str(auth_header))
-    #return HttpResponseRedirect(redirect_url)
+    # authentication failed: Go back to login
+    url = post_basf_login_callback
+    url += "?next="
+    url += quote(redirect_url, safe="")
+    return HttpResponseRedirect(url)
 
-def login(request):
-    return login_view(request)
-    """
-    if request.method == 'POST':
-        try:
-            if basf_authentification(request.POST["username"],request.POST["password"]):
-                try:
-                    user = Person.objects.filter(username=request.POST["username"])[0]
-                    django_login(request, user)
-                    return HttpResponseRedirect(request.POST["next"])
-                except Exception as e:
-                    #return render(request, 'landing_page/login_page.html', {"errors":True})
-                    return HttpResponse(str(e))
-            else:
-                return render(request, 'landing_page/login_page.html', {"errors":True,"next":request.POST["next"]})
-        except MultiValueDictKeyError:#TODO the only exception type of relevance?
-            return HttpResponse("Bad Request",status=400)
-    else:
-        return HttpResponse("Bad Request",status=400)"""
 
 def logout(request):
     django_logout(request)
