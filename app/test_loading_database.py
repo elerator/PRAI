b09230ec7649c731b2eb662203636a281fe2c570
Michael Werner Gerstenberger <GersteM5@basfad.basf.net>
2020-03-19 11:56:21 +0100

    change permissions via startup script

--- app/test_loading_database.py
+++ app/test_loading_database.py
@@ -5,4 +5,7 @@ from lightweight_research_tool.settings import *
 path = os.path.join(os.path.join(os.path.split(BASE_DIR)[0],"database"), 'db.sqlite3')
 print(path)
 
+with open(path, "r+"):
+    print("okay reading in r+ mode worked")
+
 conn = sqlite3.connect(path)
