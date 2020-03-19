b094087dcff75c7f8f4d6f2f5bf3d17ddd42e388
Michael Werner Gerstenberger <GersteM5@basfad.basf.net>
2020-03-19 10:49:30 +0100

    try changing permissions differently; try fixing missing django erro outputs

--- app/lightweight_research_tool/settings.py
+++ app/lightweight_research_tool/settings.py
@@ -33,6 +33,21 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler'
+        },
+    },
+    'loggers': {
+        '': {  # 'catch all' loggers by referencing it with the empty string
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
 
 # Application definition
 
